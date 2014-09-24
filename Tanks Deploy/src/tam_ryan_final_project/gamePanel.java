f41ff1e5bc77f5901a728872d2c502b992ff746a
Ryan <ryan.tam.24@gmail.com>
2014-09-24 12:07:02 -0400

    not quite sure what changed

--- Tanks Deploy/src/tam_ryan_final_project/gamePanel.java
+++ Tanks Deploy/src/tam_ryan_final_project/gamePanel.java
@@ -220,7 +220,7 @@ public class gamePanel extends JPanel implements KeyListener, MouseListener, Mou
                 tankX = 450;//my coridinates
                 tankY = 450;
                 activeTanks.add(new enemyTank(1150, 450, 1, false));//first enemy (coridinates, its number, weather it is smart)
-                activeBricks.add(new brick(400,500));
+                //activeBricks.add(new brick(400,500));
                 enemyNumb = 1;//number of ennemies
                 break;
             case 2:
